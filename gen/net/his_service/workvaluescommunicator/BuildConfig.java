f2b90bfc41da74b94d6ae21857beb6203de79dab
jayteesf <jayteesf>
2013-03-09 10:31:31 -0800

    initial commit ...custom form interface (in progress)

---
+++ gen/net/his_service/workvaluescommunicator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.his_service.workvaluescommunicator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
